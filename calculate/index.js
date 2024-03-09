6b8b286a46135170f455ccf228aaf35e69f8cde9
Elena <premudraae116@gmail.com>
2024-03-09 18:39:10 +0700

    Your commit message

---
+++ calculate/index.js
@@ -0,0 +1,6 @@
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles.css';
+
+import App from './Calc.js';
---
+++ src/Hello.tsx
@@ -0,0 +1,6 @@
+ function Hello(){
+    return <h1>Hello</h1>
+    let num=10;
+}
+
+export default Hello
